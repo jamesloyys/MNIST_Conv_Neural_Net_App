621ac91d7c0f6e2e3751d2763554dd07d177fc9e
James Loy <jamesloyys@gmail.com>
2017-06-15 21:30:54 +0800

    improved model accuracy

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from flask import Flask, render_template, request, jsonify
 import numpy as np
 import sys
 import tensorflow as tf
+import re
 sys.path.append('./models')
 import cnn
+from matplotlib import pyplot as plt
+from scipy.misc import imsave, imread, imresize, imshow
+import base64
+from scipy.ndimage.interpolation import zoom
+from math import sqrt
+
+# Function to convert an image from base64 into raw representation
+def convertImage(imgData1):
+	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
+	with open('./output.png','wb') as output:
+		output.write(base64.b64decode(imgstr))
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
 
 @app.route('/')
 def index():
@@ -16,9 +27,18 @@ def index():
 
 @app.route('/mnist', methods=['GET', 'POST'])
 def predict():
-	input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
+	ret = request.json
+	print(type(ret))
+	convertImage(ret)
+	input = imread('output.png',mode='L')
+	input = np.invert(input)
+	input = zoom(input, 30.0/sqrt(input.size))
+	input = np.delete(input, input.shape[0]-1, 1)
+	input = np.delete(input, 0, 1)
+	input = np.delete(input, input.shape[0]-1, 0)
+	input = np.delete(input, 0, 0)
+	input = input.reshape(1, 784)
+
 	prediction, prob_array = cnn.predict(input)
 	tf.reset_default_graph()
-	print(prob_array.tolist())
 	return jsonify(results=[prediction, prob_array.tolist()])
-
